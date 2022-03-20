9f8c4fbb8414da3db4f106d670cd22ac9687691d
yajuj <noticethatred@gmail.com>
2022-03-21 01:24:04 +0500

    created spiner component and build complite

---
+++ README.md
@@ -0,0 +1,5 @@
+# Запуск
+
+cd front npm install && npm start
+cd ..
+cd server npm install && npm start
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppContextProvider } from './context/app-context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
