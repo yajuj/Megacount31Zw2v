0b7d0f9e212574b37e3b9693fd3c13fe2e91f317
yajuj <noticethatred@gmail.com>
2022-03-21 00:27:53 +0500

    app context created

--- server/src/controllers/contacts.controller.ts
+++ server/src/controllers/contacts.controller.ts
@@ -1,37 +1,61 @@
-import { Request, Response } from 'express';
 import { db } from '..';
+import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 
-export const getContacts = async (req: Request, res: Response) => {
-  db.find({}, (err: Error | null, doc: any) => {
-    return res.json(doc);
+export const getContacts = (request: Request, response: Response) => {
+  db.find({}, function (err: Error | null, docs: any[]) {
+    if (err) {
+      return response.status(400).json({ msg: 'Произошла ошибка.' });
+    }
+    response.json(docs);
   });
 };
 
-export const updateContact = async (req: Request, res: Response) => {
-  const _id = req.params.id;
-  const data = req.body;
-  db.update({ _id }, data, { upsert: true }, (err, doc) => {
-    return res.json(doc);
-  });
-};
+export const addContact = (request: Request, response: Response) => {
+  const errors = validationResult(request);
+  if (errors.isEmpty()) {
+    return response.status(400).json({ msg: 'Заполните все поля.' });
+  }
+  const data = request.body;
 
-export const getContact = async (req: Request, res: Response) => {
-  const _id = req.params.id;
-  db.find({ _id }, (err: Error | null, doc: any) => {
-    return res.json(doc);
-  });
-};
-
-export const addContact = async (req: Request, res: Response) => {
-  const data = req.body;
   db.insert(data, (err, doc) => {
-    return res.json(doc);
+    if (err) {
+      return response
+        .status(400)
+        .json({ msg: 'Контакт с таким номером уже есть.' });
+    }
+    return response.json(doc);
   });
 };
 
-export const deleteContact = async (req: Request, res: Response) => {
-  const _id = req.params.id;
-  db.remove({ _id }, {}, function (err, numRemoved) {
-    return res.sendStatus(200);
+export const getContact = (request: Request, response: Response) => {
+  const id = request.params.id;
+  db.findOne({ _id: id }, function (err, doc) {
+    response.json(doc);
+  });
+};
+
+export const updateContact = (request: Request, response: Response) => {
+  const errors = validationResult(request);
+  if (errors.isEmpty()) {
+    return response.status(400).json({ msg: 'Заполните все поля.' });
+  }
+  const id = request.params.id;
+  const data = request.body;
+  db.update({ _id: id }, data, { upsert: true }, function (err, numReplaced) {
+    if (err) {
+      return response.status(400).json({ msg: 'Не удалось добавить контакт.' });
+    }
+    response.sendStatus(200);
+  });
+};
+
+export const deleteContact = (request: Request, response: Response) => {
+  const id = request.params.id;
+  db.remove({ _id: id }, {}, function (err, numRemoved) {
+    if (err) {
+      return response.status(400).json({ msg: 'Не удалось удалить контакт.' });
+    }
+    response.sendStatus(200);
   });
 };
